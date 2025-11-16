2dd90a6b0dfbd6a2f84ef7becb23eeaebe2e08d3
damerashirisha <damerashirisha27@gmail.com>
2025-11-16 18:41:49 +0530

    route change

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,27 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import Products from './Components/Products'
-import Home from './Components/Home'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import Product from './components/product'
-import List from './Components/Categories'
+import { useState } from "react";
+import "./App.css";
+
+import Products from "./Components/Products";
+import Home from "./Components/Home";
+import Product from "./components/product";
+import List from "./Components/Categories";
+
+import { HashRouter, Routes, Route } from "react-router-dom";
+
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
-    <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<Home/>}/>
-      <Route path='/Categories' element={<List/>}/>
-      <Route path="/product/:id" element={<Product/>}/>
-    </Routes>
-    </BrowserRouter>
+      <HashRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/Categories" element={<List />} />
+          <Route path="/product/:id" element={<Product />} />
+        </Routes>
+      </HashRouter>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
